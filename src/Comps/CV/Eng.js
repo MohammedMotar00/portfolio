f58a234d5e1d9c155f474e586ef8b0ded53b2837
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-07-20 17:55:00 +0200

    done

--- src/Comps/CV/Eng.js
+++ src/Comps/CV/Eng.js
@@ -1,7 +1,114 @@
 import React from "react";
 
 function Eng() {
-  return <div>eng cv</div>;
+  return (
+    <div>
+      <h1>MOHAMMED MOTAR</h1>
+      <hr />
+      <div>
+        <p>
+          <strong>Date of birth:</strong> 2000-02-15
+        </p>
+        <p>
+          <strong>Address:</strong> Bokvägen 15A
+        </p>
+        <p>
+          <strong>Phone number:</strong> 072 281 63 30
+        </p>
+        <p>
+          <strong>E-mail:</strong> mohammed.motar10@gmail.com
+        </p>
+        <p>
+          <strong>Extra authorization:</strong> B driving license and access to
+          car
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>EDUCATION</h3>
+        <p>
+          <strong>High school education:</strong> NTI-Gymnasiet, Kristianstad
+          2016 - 2019
+        </p>
+        <p>
+          <strong>
+            Vocational education: Frontend developer, EC-Utbildning, Helsingborg
+            2019 - 2021
+          </strong>
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>WORK EXPERIENCE</h3>
+        <p>
+          Learning at work1 and learning at work2 at Exakta Software, Hässleholm{" "}
+          <strong>(Learning at work period = 12 weeks)</strong>
+        </p>
+        <p>Internship in England for 3 weeks</p>
+        <p>Holiday work, Kristianstad 2017</p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>INTERESTS</h3>
+        <p>
+          I have a great interest in frontend development and this is something
+          I am currently passionate about. I have graduated and gained
+          experience from my Learning at work periods and I feel that I am ready
+          to give 100%. In addition to this, I have a big interest in training,
+          I usually go to the gym or play football. During my free time when I'm
+          not exercising, I usually hang out with friends or family.
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>KNOWLEDGE</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JAVASCRIPT</li>
+          <li>JQUERY</li>
+          <li>SCSS</li>
+          <li>TAILWIND CSS</li>
+          <li>BOOTSTRAP</li>
+          <li>STYLED COMPONENTS</li>
+          <li>REACT</li>
+          <li>REDUX</li>
+          <li>NEXT.JS</li>
+          <li>MATERIAL UI</li>
+          <li>VUE</li>
+          <li>VUEX</li>
+          <li>VUETIFY</li>
+          <li>PHP</li>
+          <li>LARAVEL</li>
+          <li>WORDPRESS</li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>LANGUAGE SKILLS</h3>
+        <p>Swedish - good knowledge of speech and writing</p>
+        <p>English - good knowledge of speech and writing</p>
+        <p>Arabic - good knowledge of speech and writing</p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>REFERENCES</h3>
+        <p>References available upon request</p>
+      </div>
+    </div>
+  );
 }
 
 export default Eng;
--- src/Comps/CV/Swe.js
+++ src/Comps/CV/Swe.js
@@ -1,7 +1,111 @@
 import React from "react";
 
 function Swe() {
-  return <div>swe cv</div>;
+  return (
+    <div>
+      <h1>MOHAMMED MOTAR</h1>
+      <hr />
+      <div>
+        <p>
+          <strong>Födelsedatum:</strong> 2000-02-15
+        </p>
+        <p>
+          <strong>Adress:</strong> Bokvägen 15A
+        </p>
+        <p>
+          <strong>Telefonnummer:</strong> 072 281 63 30
+        </p>
+        <p>
+          <strong>E-post:</strong> mohammed.motar10@gmail.com
+        </p>
+        <p>
+          <strong>Extra behörighet:</strong> B-körkort samt tillgång till bil
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>UTBILDNING</h3>
+        <p>
+          <strong>Gymnasieutbildning:</strong> NTI-Gymnasiet, Kristianstad 2016
+          - 2019
+        </p>
+        <p>
+          <strong>Yrkesutbildning:</strong> Frontend utvecklare, EC-Utbildning,
+          Helsingborg 2019 - 2021
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>ARBETSLIVSERFARENHET</h3>
+        <p>
+          LIA1 och LIA2 hos Exakta Software, Hässleholm{" "}
+          <strong>(LIA period = 12 veckor)</strong>
+        </p>
+        <p>APL i England för 3 veckor</p>
+        <p>Feriearbete, Kristianstad 2017</p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>INTRESSEN</h3>
+        <p>
+          Jag har ett stort intresse för frontend utveckling och detta är något
+          jag i dagsläget brinner för. Jag har lärt mig en hel del och efter att
+          jag har tagit examen och fått erfarenhet från mina LIA perioder känner
+          jag mig redo för att ge 100%. Utöver detta så har jag ett stort
+          intresse för träning, brukar gymma och spela fotboll. Under min fritid
+          när jag inte tränar så brukar jag umgås med vänner eller familj.
+        </p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>KUNSKAPER</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JAVASCRIPT</li>
+          <li>JQUERY</li>
+          <li>SCSS</li>
+          <li>TAILWIND CSS</li>
+          <li>BOOTSTRAP</li>
+          <li>STYLED COMPONENTS</li>
+          <li>REACT</li>
+          <li>REDUX</li>
+          <li>NEXT.JS</li>
+          <li>MATERIAL UI</li>
+          <li>VUE</li>
+          <li>VUEX</li>
+          <li>VUETIFY</li>
+          <li>PHP</li>
+          <li>LARAVEL</li>
+          <li>WORDPRESS</li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>SPRÅKKUNSKAPER</h3>
+        <p>Svenska - goda kunskaper i tal och skrift</p>
+        <p>Engelska - goda kunskaper i tal och skrift</p>
+        <p>Arabiska - goda kunskaper i tal och skrift</p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h3>REFERENSER</h3>
+        <p>Referenser lämnas på begäran</p>
+      </div>
+    </div>
+  );
 }
 
 export default Swe;
